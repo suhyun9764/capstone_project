1b5711172fdade1f24bf6945b8313e95cc78143c
suhyun9764 <suhyun9764@naver.com>
2023-03-23 22:08:56 +0900

    init

---
+++ src/test/java/Group2/capstone_project/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Group2.capstone_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
