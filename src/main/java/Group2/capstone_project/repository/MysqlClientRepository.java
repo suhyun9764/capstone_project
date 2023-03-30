13bbdd3b7af1128e64a1916915bb69f39e590eb2
Suhyun Shin <suhyun9764@naver.com>
2023-03-30 23:36:39 +0900

    connect mysql

--- src/main/java/Group2/capstone_project/repository/MysqlClientRepository.java
+++ src/main/java/Group2/capstone_project/repository/MysqlClientRepository.java
@@ -2,8 +2,11 @@ package Group2.capstone_project.repository;
 
 import Group2.capstone_project.domain.Client;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,21 +25,42 @@ public class MysqlClientRepository implements ClientRepository{
 
     @Override
     public Optional<Client> findId(String name, String studentNumber, String age) {
-        return Optional.empty();
+        String sql = "SELECT * FROM client where name = name and studentNumber = studentNumber and age = age";
+        List<Client> result = jdbcTemplate.query(sql,clientRowMapper());
+        return result.stream().findAny();
+
     }
 
     @Override
     public Optional<Client> findPwd(String name, String id, String studentNumber) {
-        return Optional.empty();
+        String sql = "SELECT * FROM client where name = name and id = id and studentNumber = studentNumber";
+        List<Client> result = jdbcTemplate.query(sql,clientRowMapper());
+        return result.stream().findAny();
     }
 
     @Override
     public List<Client> findAll() {
-        return null;
+        String sql = "SELECT * FROM client";
+        return jdbcTemplate.query(sql,clientRowMapper());
     }
 
     @Override
     public Optional<Client> findById(String id) {
         return Optional.empty();
+    }
+
+    private RowMapper<Client> clientRowMapper(){
+        return (rs, rowNum) -> {
+
+            Client client = new Client();
+            client.setId(rs.getString("id"));
+            client.setName(rs.getString("name"));
+            client.setStudentNumber(rs.getString("studentNumber"));
+            client.setAge(rs.getString("age"));
+            client.setPwd(rs.getString("pwd  "));
+
+
+            return client;
+        };
     }
 }
